252368a62130b7a2a3b78dee7f3cbdf2bdf1a7bf
Alex Bublikov <madsilver925@gmail.com>
2020-07-11 20:15:35 +0300

    add task1

--- HomeWork1/part1/biblio.py
+++ HomeWork1/part1/biblio.py
@@ -7,9 +7,9 @@ class BiblioClass:
     def c_res(self):
         list_book = []
         list_user = []
-        with open('data/users.json', "r") as json_file:
+        with open('../data/users.json', "r") as json_file:
             data_user = json.load(json_file)
-        with open('data/books.csv', 'r') as csv_file:
+        with open('../data/books.csv', 'r') as csv_file:
             data_book = csv.DictReader(csv_file)
             for item in data_book:
                 list_book.append(item)
@@ -23,7 +23,7 @@ class BiblioClass:
         elif len(data_user) > len(list_book):
             for item in data_user:
                 book_it = next(it)
-                if next(it) is not None:
+                if book_it is not None:
                     res = dict(name=item['name'], gender=item['gender'], address=item['address'],
                                book=[dict(title=book_it['Title'], author=book_it['Author'], height=book_it['Height'])])
                 else:
@@ -31,7 +31,7 @@ class BiblioClass:
                            book=[dict()])
                 list_user.append(res)
         r = list_user
-        with open('data/result.json', "w") as file:
+        with open('../data/result.json', "w") as file:
             s = json.dumps(list_user, indent=4)
             file.write(s)
         print("SUCCSESS")
