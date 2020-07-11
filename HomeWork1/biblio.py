b3de60aae2e4bef42d6cd6becdaeab75a5d63fcf
Alex Bublikov <madsilver925@gmail.com>
2020-07-11 13:01:56 +0300

    task 2 context manager

---
+++ HomeWork1/biblio.py
@@ -0,0 +1,45 @@
+import csv
+import json
+
+
+class BiblioClass:
+
+    def c_res(self):
+        list_book = []
+        list_user = []
+        with open('data/users.json', "r") as json_file:
+            data_user = json.load(json_file)
+        with open('data/books.csv', 'r') as csv_file:
+            data_book = csv.DictReader(csv_file)
+            for item in data_book:
+                list_book.append(item)
+        it = iter(list_book)
+        if len(data_user) < len(list_book):
+            for item in data_user:
+                book_it = next(it)
+                res = dict(name=item['name'], gender=item['gender'], address=item['address'],
+                           book=[dict(title=book_it['Title'], author=book_it['Author'], height=book_it['Height'])])
+                list_user.append(res)
+        elif len(data_user) > len(list_book):
+            for item in data_user:
+                book_it = next(it)
+                if next(it) is not None:
+                    res = dict(name=item['name'], gender=item['gender'], address=item['address'],
+                               book=[dict(title=book_it['Title'], author=book_it['Author'], height=book_it['Height'])])
+                else:
+                    res = dict(name=item['name'], gender=item['gender'], address=item['address'],
+                           book=[dict(title=book_it['Title'], author=book_it['Author'], height=book_it['Height'])])
+                list_user.append(res)
+        r = list_user
+        with open('data/result.json', "w") as file:
+            s = json.dumps(list_user, indent=4)
+            file.write(s)
+        print("SUCCSESS")
+
+
+    # #     print(str(e))
+
+
+b = BiblioClass()
+b.c_res()
+# b.create_result()
